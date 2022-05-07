40ce59b90a6cc957ad636774723f500d7af961a1
Robin Windey <ro.windey@gmail.com>
2022-05-07 09:31:03 +0200

    NC24 compat (#88)

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -1,21 +1,15 @@
 <?php
 
-use OCA\Files_PhotoSpheres\AppInfo;
-
-if (!defined('PHPUNIT_RUN')) {
-	define('PHPUNIT_RUN', 1);
-}
-
-require_once __DIR__.'/../../../lib/base.php';
+require_once __DIR__ . '/../../../tests/bootstrap.php';
 
 // Fix for "Autoload path not allowed: .../tests/lib/testcase.php"
 \OC::$loader->addValidRoot(OC::$SERVERROOT . '/tests');
 
-// Fix for "Autoload path not allowed: .../files_photospheres/tests/testcase.php"
-\OC_App::loadApp(AppInfo\Application::APP_NAME);
-
 if (!class_exists('PHPUnit\Framework\TestCase')) {
 	require_once('PHPUnit/Autoload.php');
 }
+
+// Fix for "Autoload path not allowed: .../files_photospheres/tests/testcase.php"
+\OC_App::loadApp('files_photospheres');
 
 OC_Hook::clear();
