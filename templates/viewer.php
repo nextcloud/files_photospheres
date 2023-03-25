ca2906f5e532be3f44b7b4561a2e0b17fdb19d2f
Robin Windey <ro.windey@gmail.com>
2023-03-25 12:30:09 +0100

    NC 26 compat (#107)
    * Make compatible with Nextcloud 26
    * Deprecate PHP 7.4, add support for PHP 8.2
    * Use new Photosphere-Viewer version 5.15.1

--- templates/viewer.php
+++ templates/viewer.php
@@ -13,15 +13,14 @@
         <meta http-equiv="X-UA-Compatible" content="IE=edge">
         <meta name="viewport" content="width=device-width, initial-scale=1.0, minimum-scale=1.0">
         <link rel="stylesheet" href="<?php p($urlGenerator->linkTo(AppInfo\Application::APP_NAME, 'css/viewer.css')) ?>?v=<?php p($version) ?>"/>
-        <link rel="stylesheet" href="<?php p($urlGenerator->linkTo(AppInfo\Application::APP_NAME, 'css/photo-sphere-viewer.min.css')) ?>?v=<?php p($version) ?>"/>
-        <script nonce="<?php p($nounceManager->getNonce()) ?>" src="<?php p($urlGenerator->linkTo(AppInfo\Application::APP_NAME, 'js/three.min.js')) ?>?v=<?php p($version) ?>"></script>
-        <script nonce="<?php p($nounceManager->getNonce()) ?>" src="<?php p($urlGenerator->linkTo(AppInfo\Application::APP_NAME, 'js/browser.min.js')) ?>?v=<?php p($version) ?>"></script>
-        <script nonce="<?php p($nounceManager->getNonce()) ?>" src="<?php p($urlGenerator->linkTo(AppInfo\Application::APP_NAME, 'js/photo-sphere-viewer.min.js')) ?>?v=<?php p($version) ?>"></script>
-        <script nonce="<?php p($nounceManager->getNonce()) ?>" src="<?php p($urlGenerator->linkTo(AppInfo\Application::APP_NAME, 'js/init.js')) ?>?v=<?php p($version) ?>"></script>
+        <link rel="stylesheet" href="<?php p($urlGenerator->linkTo(AppInfo\Application::APP_NAME, 'css/photo-sphere-viewer/core/index.min.css')) ?>?v=<?php p($version) ?>"/>
+        <script nonce="<?php p($nounceManager->getNonce()) ?>" src="<?php p($urlGenerator->linkTo(AppInfo\Application::APP_NAME, 'js/photo-sphere-viewer/three.min.js')) ?>?v=<?php p($version) ?>"></script>
+		<script nonce="<?php p($nounceManager->getNonce()) ?>" src="<?php p($urlGenerator->linkTo(AppInfo\Application::APP_NAME, 'js/photo-sphere-viewer/core/index.min.js')) ?>?v=<?php p($version) ?>"></script>
+		<script nonce="<?php p($nounceManager->getNonce()) ?>" src="<?php p($urlGenerator->linkTo(AppInfo\Application::APP_NAME, 'js/photo-sphere-viewer/gyroscope-plugin/index.min.js')) ?>?v=<?php p($version) ?>"></script>
+		<script nonce="<?php p($nounceManager->getNonce()) ?>" src="<?php p($urlGenerator->linkTo(AppInfo\Application::APP_NAME, 'js/photo-sphere-viewer/stereo-plugin/index.min.js')) ?>?v=<?php p($version) ?>"></script>
+		<script nonce="<?php p($nounceManager->getNonce()) ?>" src="<?php p($urlGenerator->linkTo(AppInfo\Application::APP_NAME, 'js/init.js')) ?>?v=<?php p($version) ?>"></script>
         <script nonce="<?php p($nounceManager->getNonce()) ?>" src="<?php p($urlGenerator->linkTo(AppInfo\Application::APP_NAME, 'js/functions.js')) ?>?v=<?php p($version) ?>"></script>
-        <script nonce="<?php p($nounceManager->getNonce()) ?>" src="<?php p($urlGenerator->linkTo(AppInfo\Application::APP_NAME, 'js/gyroscope.min.js')) ?>?v=<?php p($version) ?>"></script>
-        <script nonce="<?php p($nounceManager->getNonce()) ?>" src="<?php p($urlGenerator->linkTo(AppInfo\Application::APP_NAME, 'js/stereo.min.js')) ?>?v=<?php p($version) ?>"></script>
-    </head>
+	</head>
     <body id="body-public">
         <div id="viewer"></div>
     </body>
