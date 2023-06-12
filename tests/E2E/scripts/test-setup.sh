09fd32113cf5ebf10daeb87842a601da822813be
Robin Windey <ro.windey@gmail.com>
2023-06-12 19:27:12 +0200

    Add Playwright E2E tests

---
+++ tests/E2E/scripts/test-setup.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+set -euo pipefail
+
+SCRIPT_DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" >/dev/null 2>&1 && pwd )"
+
+. "$SCRIPT_DIR/vars.sh"
+
+curl --fail-with-body -u $E2E_USER:$E2E_PASSWORD -X MKCOL "${E2E_BASE_URL}/ppv-testfiles"
+curl --fail-with-body -u $E2E_USER:$E2E_PASSWORD -T "${SCRIPT_DIR}/../testdata/pano.jpg" "${E2E_BASE_URL}/ppv-testfiles/pano.jpg"
+curl --fail-with-body -u $E2E_USER:$E2E_PASSWORD -T "${SCRIPT_DIR}/../testdata/non-pano.jpg" "${E2E_BASE_URL}/ppv-testfiles/non-pano.jpg"
